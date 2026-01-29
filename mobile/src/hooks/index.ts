fa55531a327c29fa365f2468a72fa7ec10ac31dc
Denis Ruiz <denisruiz112789@gmail.com>
2026-01-28 21:23:49 -0500

    Nuevo

--- mobile/src/hooks/index.ts
+++ mobile/src/hooks/index.ts
@@ -1,3 +1,1 @@
-export { useProducts } from './useProducts'
 export { useOrders, useMyOrderHistory, useOrderDetail } from './useOrders'
-export { usePagination, useInfiniteList } from './usePagination'
